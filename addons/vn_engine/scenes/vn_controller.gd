80428d2b6b143d1e2b56acdcf7970d7f9c94a5e4
Boss <bossnathakorn@gmail.com>
2023-04-02 16:40:48 +0700

    impl display dialog text

--- addons/vn_engine/scenes/vn_controller.gd
+++ addons/vn_engine/scenes/vn_controller.gd
@@ -5,14 +5,25 @@ extends Node
 class_name VNController
 
 @export_category("Objects")
-@export var ui_control: Control
+@export var vn_player_scn: PackedScene
+@onready var vn_player: VNPlayer = vn_player_scn.instantiate() as VNPlayer
 
 @export_category("State")
 @export var initial_scene: VNScene
 
 var current_scene: VNScene:
 	set(new_scene):
-		print("Transition scene %s to %s" % [current_scene.id, new_scene.id])
+		print("Transition scene to %s" % new_scene.id)
 		current_scene = new_scene
 
+func _ready() -> void:
+	add_child(vn_player)
+	current_scene = initial_scene
+	var scn := current_scene.flows[0] as VNDialog
+	vn_player.show_dialog("Test speaker", scn.dialog)
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		var key_event := event as InputEventKey
+		if key_event.keycode == KEY_SPACE:
+			vn_player.force_show_full_dialog()
--- addons/vn_engine/scenes/vn_player.gd
+++ addons/vn_engine/scenes/vn_player.gd
@@ -2,5 +2,50 @@ extends Control
 
 class_name VNPlayer
 
+@export_category("Dialog Config")
+## Character delay when displaying in millisec
+@export_range(0, 300, 1, "or_greater", "suffix") var char_delay: int = 25
+
 @onready var speaker_label: Label = $VBoxContainer/SpeakerLabel
 @onready var dialog_text: RichTextLabel = $VBoxContainer/DialogBox/MarginContainer/DialogText
+
+@onready var _dialog_display_timer: Timer = $DialogDisplayTimer
+
+var _original_text: String
+
+var _char_index: int
+var _current_text: String
+
+func _ready() -> void:
+	# Ensure timer
+	_dialog_display_timer.one_shot = true
+	_dialog_display_timer.autostart = false
+	_dialog_display_timer.wait_time = float(char_delay) / 1000
+
+func show_dialog(speaker: String, text: String) -> void:
+	speaker_label.text = speaker
+	_original_text = text
+	_current_text = ""
+
+	update_dialog_box()
+	_flow_dialog()
+
+func force_show_full_dialog() -> void:
+	if not _dialog_display_timer.is_stopped():
+		_dialog_display_timer.stop()
+	_current_text = _original_text
+	update_dialog_box()
+
+func _flow_dialog() -> void:
+	_dialog_display_timer.start()
+
+func update_dialog_box() -> void:
+	dialog_text.text = _current_text
+
+func _on_dialog_display_timer_timeout() -> void:
+	if _current_text != _original_text:
+		var next_char := _original_text.substr(_char_index, 1)
+		_current_text += next_char
+		update_dialog_box()
+		_char_index += 1
+		_dialog_display_timer.start()
