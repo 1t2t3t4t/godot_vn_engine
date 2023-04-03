85097ff52cc871ed87bd724ec5869316f3e96a2d
Boss <bossnathakorn@gmail.com>
2023-04-03 19:35:19 +0700

    update default

--- addons/vn_engine/scenes/vn_controller.gd
+++ addons/vn_engine/scenes/vn_controller.gd
@@ -6,11 +6,15 @@ class_name VNController
 
 @export_category("Objects")
 @export var vn_player_scn: PackedScene
-@export var config: VNConfig
-@onready var vn_player: VNPlayer = vn_player_scn.instantiate() as VNPlayer
+@export var config: VNConfig:
+	set(new_config):
+		config = new_config
+		update_configuration_warnings()
 
 @export_category("State")
 @export var initial_scene: VNScene
+
+@onready var vn_player: VNPlayer = vn_player_scn.instantiate() as VNPlayer
 
 var current_scene: VNScene:
 	set(new_scene):
@@ -23,3 +27,9 @@ func _ready() -> void:
 	current_scene = initial_scene
 	var scn := current_scene.flows[0] as VNDialog
 	vn_player.show_dialog("Test speaker", scn.dialog)
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var result: Array[String] = []
+	if config == null:
+		result.append("VNConfig is empty")
+	return PackedStringArray(result)
